557f279029e692f60c19c9995fffb2dfde21c30d
Cecile <seveninjuly@gmail.com>
2019-07-27 21:59:36 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+$(function shoppinglist() {
+	$('#js-shopping-list-form').submit(event => {
+		event.preventDefault();
+		
+		$('#shopping-list-entry').empty();
+		
+		const listEntry = $('#shopping-list-entry').val();
+
+		$('.shopping-list').append(
+			`<li>
+        <span class="shopping-item">${listEntry}</span>
+        <div class="shopping-item-controls">
+           <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+           </button>
+           <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+           </button>
+        </div>
+       </li>`);
+    });
+
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+	   $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+
+    });
+
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+	    $(this).closest('li').remove();
+	});
+});
